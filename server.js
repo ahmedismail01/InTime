58019f90f256554ae1844f510024c47cd98e40b7
ahmedismail01 <ahmed.ism1990.ai@gmail.com>
2024-03-02 01:02:19 +0200

    cors

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const app = express();
 const connection = require("./config/dbConnection");
 const routes = require("./router/index");
+const cors = require('cors')
 
+
+app.use(cors())
 app.use(express.json());
 app.use(routes);
+
 connection().then(() => app.listen("8080", () => console.log("server listening at port 8080")));
