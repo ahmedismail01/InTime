5d0af6ee87c7fc757db8bc2c916aa353328460e2
ahmedismail01 <ahmed.ism1990.ai@gmail.com>
2024-04-27 19:21:54 +0300

    pagination fix

--- utils/paginate.js
+++ utils/paginate.js
@@ -1,13 +1,8 @@
-// 2 3
-
-// 2 * 2 4 -2 
 module.exports = paginate = (itemsPerPage, page, items) => {
-  const start = (page == 1) ? 0 : (page * itemsPerPage) - itemsPerPage;
+  const start = page == 1 ? 0 : page * itemsPerPage - itemsPerPage;
   const end = start + itemsPerPage;
-  const paginatedItems = items.slice(start, end)
-  const previousPage = items[start - 1] ? true : false
-  const nextPage =  items[end + 1] ? true : false
-  return {paginatedItems , previousPage , nextPage}
+  const paginatedItems = items.slice(start, end);
+  const previousPage = items[start - 1] ? true : false;
+  const nextPage = items[end] ? true : false;
+  return { paginatedItems, previousPage, nextPage };
 };
-
-
