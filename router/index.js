b04919d5a9db1043ec4141287744a0d5e22aca64
ahmed ismail <ahmed.ism1990.ai@gmail.com>
2026-05-23 00:15:16 +0300

    added rate limit

--- router/index.js
+++ router/index.js
@@ -2,8 +2,9 @@ const app = require("express").Router();
 const userRoutes = require("./user/index");
 const authRoutes = require("./auth");
 const imageRoutes = require("./images");
+const { authLimiter } = require("../utils/rateLimiter");
 app.use("/user", userRoutes);
-app.use("/auth", authRoutes);
+app.use("/auth" , authLimiter, authRoutes);
 app.use("/images", imageRoutes);
 
 app.all("*", (req, res, next) => {
--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const cors = require("cors");
 const morgan = require("morgan");
 const routes = require("./router/index");
 const { scheduleTasks } = require("./helpers/scheduler/tasks");
+const { limiter } = require("./utils/rateLimiter");
 
 scheduleTasks();
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1/", routes);
+app.use("/api/v1/", limiter, routes);
 
 connection().then(() =>
-  server.listen("8080", () => console.log("server listening at port 8080"))
+  server.listen("8080", () => console.log("server listening at port 8080")),
 );
